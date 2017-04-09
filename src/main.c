984292851eeaebead0773bff09c69cf77a80c63a
Jarosław Wieczorek <jaroslaw.wieczorek@sealcode.org>
2017-04-09 15:18:26 +0200

    "Test commit & change autor"

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 /**
   ******************************************************************************
   * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
+  * @author  Jarosław Wieczorek
+  * @version V0.1
+  * @date    09-April-2017
   * @brief   Default main function.
   ******************************************************************************
 */
@@ -11,7 +11,17 @@
 
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
-			
+#include "stm32f4xx_adc.h"
+#include "stm32f4xx_dac.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_exti.h"
+#include "stm32f4xx_i2c.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_usart.h"
+#include "stm32f4xx_tim.h"
+#include "stm32f4xx_spi.h"
+#include "stm32f4xx_conf.h"
+#include "misc.h"
 
 int main(void)
 {
